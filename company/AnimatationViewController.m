4f24cbc5192d157b3a9e66066e275b998c0dc2c7
单小飞 <2530101715@qq.com>
2016-07-29 11:00:30 +0800

    11:00

--- company/AnimatationViewController.m
+++ company/AnimatationViewController.m
@@ -35,10 +35,103 @@
 
 @implementation AnimatationViewController
 
+- (void)setAsycnBtn:(UIButton *)asycnBtn{
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+           self.targetImageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://new-img2.ol-img.com/90x64/114/192/liPpATxvC0196.gif"]]];
+    });
+    
+    [UIView animateWithDuration:2.0 animations:^{
+        
+    } completion:^(BOOL finished) {
+        
+    }];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//LoadimageView
+-(UIImage *)loadimage{
+    
+    NSString *path =  [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSSystemDomainMask, YES).firstObject stringByAppendingString:@"/image"];
+    NSFileManager *manage = [NSFileManager defaultManager];
+    if (![manage fileExistsAtPath:path])//判读文件是否存在 如果不存在
+    {
+        NSLog(@"imageFile is not exist");
+  __block  BOOL isSuccess;
+        dispatch_async(dispatch_get_main_queue(), ^{//异步下载图片 防止主线程阻塞
+//                UIImage *image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://new-img2.ol-img.com/90x64/114/192/liPpATxvC0196.gif"]]];
+            NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://new-img2.ol-img.com/90x64/114/192/liPpATxvC0196.gif"]];
+            isSuccess  = [data writeToFile:path atomically:YES];
+            if (isSuccess)
+            {
+                self.targetImageView.image= [UIImage imageWithData:data];
+            }else
+            {
+                UILabel *lab = [UILabel new];
+                lab.frame = CGRectMake((self.view.frame.size.width-60)/2.0, (self.view.frame.size.height-40)/2.0, 100, 40);
+                lab.text = @"图片写入失败";
+                lab.backgroundColor= [UIColor grayColor];
+                lab.alpha = 0.5;
+                [UIView animateWithDuration:2.0 animations:^{
+                     [self.view addSubview:lab];
+                } completion:^(BOOL finished) {
+                    [lab removeFromSuperview];
+                }];
+            }
+        });
+    
+        
+    }
+    
+    return [UIImage new];
+}
+
+
+
+
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    
+//    要操作的Animatation目标
+    [self loadimage];
 }
 
 - (void)didReceiveMemoryWarning {
